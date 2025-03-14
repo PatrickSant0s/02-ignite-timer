f44eaf76306812bbd05bcd547bcdfe99a034b468
Patrick Santos <Patricksantosribeiro2017@gmail.com>
2025-03-13 21:33:00 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/Button";
+
+export function App() {
+  return (
+    <>
+      <Button color="primary"></Button>
+      <Button color="secondary"></Button>
+      <Button color="danger"></Button>
+      <Button color="success"></Button>
+    </>
+  );
+}
---
+++ src/components/Button.styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+  width: 150px;
+  height: 50px;
+`;
